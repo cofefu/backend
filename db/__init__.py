5aff73b09b5ea4187b2b4d49a44cfaab1a221bac
hedgehog319 <fign1910@mail.ru>
2021-12-23 13:19:46 +1000

    * Change BaseModel.data method

--- db/__init__.py
+++ db/__init__.py
@@ -13,11 +13,12 @@ db = SqliteDatabase(os.path.join(BASE_DIR, DATABASE['NAME']))
 class BaseModel(Model):
     def data(self, **kwargs) -> dict:
         """Returns a dictionary with fields, excluding hidden_fields"""
-        hidden = getattr(self._meta, 'hidden_fields', None)
-        # + kwargs.get('hidden_fields', None) # TODO
-        if hidden is None:
+        fields = set(self.__data__.keys()).union(set(kwargs.pop('fields', '')))
+        fields -= set(kwargs.pop('hide', ''))
+
+        if fields is None:
             return self.__data__
-        return {k: v for k, v in self.__data__.items() if k not in hidden}
+        return {k: v for k, v in self.__data__.items() if k in fields}
 
     class Meta:
         database = db
