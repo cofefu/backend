de7fa75778f0f761d43e0796c0d445da328a8dce
hedgehog319 <fign1910@mail.ru>
2022-01-19 15:29:49 +1000

    * Change run command

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ args = parser.parse_args()
 
 def main():
     if args.runserver:
-        os.system('uvicorn backend.server:app --host 0.0.0.0 --reload')
+        os.system('uvicorn server:app --host 0.0.0.0 --reload')
 
 
 if __name__ == '__main__':
