026d033c6e3d20b3b8b47660a03e23e6e88434e4
hedgehog319 <fign1910@mail.ru>
2022-01-19 15:28:11 +1000

    - Remove comments

--- backend/settings.py
+++ backend/settings.py
@@ -8,10 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASE = {
-    # 'ENGINE': 'postgresql',
     'NAME': os.getenv("DB_NAME"),
     'USER': os.getenv("DB_USER"),
     'PASSWORD': os.getenv("DB_PASSWORD"),
-    # 'HOST': 'localhost',
-    # 'PORT': '5432',
 }
