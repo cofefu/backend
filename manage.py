9c95b6b843099f62cdfcf162238808f567ff4519
hedgehog319 <fign1910@mail.ru>
2022-04-12 16:04:30 +1000

    * change launch port

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ def main():
     if 'runserver' in sys.argv:
         uvicorn.run(
             'main:app',
-            port=8001,
+            port=8000,
             # ssl_certfile=WEBHOOK_SSL_CERT,
             # ssl_keyfile=WEBHOOK_SSL_PRIV,
             log_level='info',
