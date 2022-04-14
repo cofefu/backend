1a6745bde315ec94f4ccf2190b255b2697662aed
hedgehog319 <fign1910@mail.ru>
2022-04-14 22:22:17 +1000

    * Update manage.py

--- manage.py
+++ manage.py
@@ -1,17 +1,20 @@
 # TODO implement the module
-import sys
+import argparse
 import uvicorn
+
+# init parser
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('--runserver', action='store_true')
+args = parser.parse_args()
 
 
 def main():
-    if 'runserver' in sys.argv:
+    if args.runserver:
         uvicorn.run(
             'main:app',
             port=8000,
-            # ssl_certfile=WEBHOOK_SSL_CERT,
-            # ssl_keyfile=WEBHOOK_SSL_PRIV,
             log_level='info',
-            reload=True,
+            workers=1,
         )
 
 
