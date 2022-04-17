c0196537864a42d97ff5510ca1bff734a4d48953
hedgehog319 <fign1910@mail.ru>
2022-04-17 14:50:51 +1000

    + Create tests

---
+++ tests/test_api.py
@@ -0,0 +1,64 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_get_products():
+    pass
+
+
+def test_get_coffee_houses():
+    pass
+
+
+def test_get_order_status():
+    pass
+
+
+def test_get_products_various():
+    pass
+
+
+def test_get_toppings():
+    pass
+
+
+# def test_get_favicon_svg():
+#     pass
+
+
+# def test_get_favicon_ico():
+#     pass
+
+
+def test_post_make_order():
+    pass
+
+
+def test_post_register_customer():
+    pass
+
+
+def test_post_update_token():
+    pass
+
+
+def test_pest_send_login_code():
+    pass
+
+
+def testp_get_verify_login_code():
+    pass
+
+
+def test_get_my_orders():
+    pass
+
+
+def test_get_me():
+    pass
+
+
+def test_get_last_order():
+    pass
