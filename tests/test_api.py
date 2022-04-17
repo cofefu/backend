d0fc89245ed55b942c159a9778750874feda35a8
hedgehog319 <fign1910@mail.ru>
2022-04-17 15:12:39 +1000

    * update

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,60 +5,75 @@ client = TestClient(app)
 
 
 def test_get_products():
-    pass
+    responce = client.get('/products')
+    assert response.status_code == 200
 
 
 def test_get_coffee_houses():
-    pass
+    responce = client.get('/coffee_houses')
+    assert response.status_code == 200
 
 
 def test_get_order_status():
-    pass
+    responce = client.get('/order_status')
+    assert response.status_code == 200
 
 
 def test_get_products_various():
-    pass
+    responce = client.get('/products_various/1')
+    assert response.status_code == 200
 
 
 def test_get_toppings():
-    pass
+    responce = client.get('/products_various/1')
+    assert response.status_code == 200
 
 
 # def test_get_favicon_svg():
-#     pass
+#     responce = client.get('/')
+#     assert response.status_code == 200
 
 
 # def test_get_favicon_ico():
-#     pass
+#     responce = client.get('/')
+#     assert response.status_code == 200
 
 
 def test_post_make_order():
-    pass
+    responce = client.post('/make_order')
+    assert response.status_code == 200
 
 
 def test_post_register_customer():
-    pass
+    responce = client.post('/register_customer')
+    assert response.status_code == 200
 
 
 def test_post_update_token():
-    pass
+    responce = client.post('/update_token')
+    assert response.status_code == 200
 
 
-def test_pest_send_login_code():
-    pass
+def test_post_send_login_code():
+    responce = client.post('/send_login_code')
+    assert response.status_code == 200
 
 
 def testp_get_verify_login_code():
-    pass
+    responce = client.get('/verify_login_code')
+    assert response.status_code == 200
 
 
 def test_get_my_orders():
-    pass
+    responce = client.get('/my_orders')
+    assert response.status_code == 200
 
 
 def test_get_me():
-    pass
+    responce = client.get('/me')
+    assert response.status_code == 200
 
 
 def test_get_last_order():
-    pass
+    responce = client.get('/last_order')
+    assert response.status_code == 200
