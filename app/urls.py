2b3b2d22c74ed5f0e89f872dd37a63083180138c
hedgehog319 <fign1910@mail.ru>
2022-05-03 22:34:55 +1000

    * add auto app loader

--- app/urls.py
+++ app/urls.py
@@ -15,7 +15,7 @@ from fastapiProject.settings import JWT_SECRET_KEY, JWT_ALGORITHM
 from bot.bot_funcs import send_order, send_login_code, send_feedback_to_telegram, send_bugreport_to_telegram
 from app.dependencies import get_current_active_user, get_current_user, get_not_baned_user, timeout_is_over
 
-router = APIRouter()
+router = APIRouter(prefix='/api')
 
 
 def create_token(customer: Customer) -> str:
