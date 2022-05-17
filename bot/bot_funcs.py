a678593fc06cd01dd8698e1a33fcf22fc9656ae8
4qwerty1 <prosin270901@gmail.com>
2022-05-17 21:31:18 +1000

    cosmetic changes

--- bot/bot_funcs.py
+++ bot/bot_funcs.py
@@ -51,7 +51,7 @@ def send_feedback_to_telegram(msg: str):
 def send_bugreport_to_telegram(msg: str, customer: Customer = None):
     msg_text = '<b>BUG REPORT</b>\n' + msg
     if customer:
-        msg_text += f'\n<b>Номер телефона:</b> {customer.phone_number}'
+        msg_text += f'\n<b>Номер телефона:</b> 8{customer.phone_number}'
     else:
         msg_text += f'\n<b>Пользователь не зарегистрирован</b>'
     bot.send_message(chat_id=FEEDBACK_CHAT, text=msg_text, parse_mode='HTML')
