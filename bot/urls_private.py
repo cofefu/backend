eb67a5938ff15c883ca807ed12a1300502c0b1c6
4qwerty1 <prosin270901@gmail.com>
2022-05-22 13:18:55 +1000

    test new FSM for change_name command

--- bot/urls_private.py
+++ bot/urls_private.py
@@ -1,33 +1,13 @@
 from bot import bot
 from telebot import types
 
-from app.models import Customer
+from app.models import Customer, FSM
 from bot.bot_funcs import send_bugreport_to_telegram, send_feedback_to_telegram
-
-
-def change_user_name_state(message: types.Message, customer: Customer):
-    new_name = message.text.strip()
-    if not new_name:
-        bot.send_message(chat_id=message.chat.id, text='Имя не может быть пустым.')
-        bot.register_next_step_handler_by_chat_id(message.chat.id, change_user_name_state, customer)
-        return
-    if len(new_name) > 20:
-        bot.send_message(chat_id=message.chat.id, text='Длина имени не должна превышать 20 символов.')
-        bot.register_next_step_handler_by_chat_id(message.chat.id, change_user_name_state, customer)
-        return
-
-    customer.name = new_name
-    customer.save()
-    bot.send_message(chat_id=message.chat.id,
-                     text=f'Имя пользователя обновлено.\nНовое имя пользователя: {customer.name}')
+from bot.filters import States
 
 
 def bug_report_state(message: types.Message, customer: Customer = None):
     send_bugreport_to_telegram(message.text.strip(), customer=customer)
-
-
-def feed_back_state(message: types.Message):
-    send_feedback_to_telegram(message.text.strip())
 
 
 def send_contact_state(message: types.Message):
@@ -36,7 +16,7 @@ def send_contact_state(message: types.Message):
         bot.register_next_step_handler_by_chat_id(message.chat.id, bug_report_state, customer)
     else:
         bot.register_next_step_handler_by_chat_id(message.chat.id, bug_report_state)
-    bot.send_message(message.chat.id, text='Введите ваше сообщение:', reply_markup=types.ReplyKeyboardRemove())
+    bot.send_message(message.chat.id, text='Введите ваше сообщение:')
 
 
 @bot.message_handler(commands=['help'], chat_types=['private'])
@@ -48,16 +28,6 @@ def send_help_info(message):
     msg += '<b>/feed_back</b> - <i>для советов, пожеланий ;)</i>'
 
     bot.send_message(chat_id=message.chat.id, text=msg, parse_mode='HTML')
-
-
-@bot.message_handler(commands=['change_name'], chat_types=['private'])
-def handle_change_name_command(message):
-    if customer := Customer.get_or_none(Customer.telegram_id == message.from_user.id):
-        bot.send_message(message.chat.id, text='Введите новое имя (не более 20 символов):')
-        bot.register_next_step_handler_by_chat_id(message.chat.id, change_user_name_state, customer)
-    else:
-        bot.send_message(chat_id=message.chat.id,
-                         text='Пользователь не найден. Пожалуйста, еще раз подтвердите номер телефона (команда /start)')
 
 
 @bot.message_handler(content_types=['contact'], chat_types=['private'])
@@ -83,10 +53,22 @@ def contact_handler(message):
                          text='Пользователь с таким номером телефона не найден.')
 
 
+@bot.message_handler(commands=['change_name'], chat_types=['private'])
+def handle_change_name_command(message):
+    if Customer.get_or_none(Customer.telegram_id == message.from_user.id):
+        fsm: FSM = FSM.get_or_create(telegram_id=message.from_user.id)[0]
+        fsm.state = States.changing_name
+        fsm.save()
+        bot.send_message(message.chat.id, text='Введите новое имя (не более 20 символов):')
+    else:
+        bot.send_message(chat_id=message.chat.id,
+                         text='Пользователь не найден. Пожалуйста, еще раз подтвердите номер телефона (команда /start)')
+
+
 @bot.message_handler(commands=['bug_report'], chat_types=['private'])
 def handler_bug_report_command(message: types.Message):
     if Customer.get_or_none(Customer.telegram_id == message.from_user.id):
-        markup = types.ReplyKeyboardMarkup(row_width=2)
+        markup = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
         markup.add(types.KeyboardButton('Да'))
         markup.add(types.KeyboardButton('Нет'))
         bot.send_message(chat_id=message.chat.id,
@@ -102,3 +84,30 @@ def handler_bug_report_command(message: types.Message):
 def handler_feed_back_command(message):
     bot.send_message(chat_id=message.chat.id, text='Введите ваше сообщение:')
     bot.register_next_step_handler_by_chat_id(message.chat.id, feed_back_state)
+
+
+@bot.message_handler(func=lambda m: True, state=States.changing_name)
+def change_user_name_state(message: types.Message):
+    customer: Customer = Customer.get(Customer.telegram_id == message.from_user.id)
+    new_name = message.text.strip()
+    if not new_name:
+        bot.send_message(chat_id=message.chat.id, text='Имя не может быть пустым.')
+        bot.register_next_step_handler_by_chat_id(message.chat.id, change_user_name_state, customer)
+        return
+    if len(new_name) > 20:
+        bot.send_message(chat_id=message.chat.id, text='Длина имени не должна превышать 20 символов.')
+        bot.register_next_step_handler_by_chat_id(message.chat.id, change_user_name_state, customer)
+        return
+
+    customer.name = new_name
+    customer.save()
+    fsm: FSM = FSM.get(FSM.telegram_id == message.from_user.id)
+    fsm.state = None
+    fsm.save()
+    bot.send_message(chat_id=message.chat.id,
+                     text=f'Имя пользователя обновлено.\nНовое имя пользователя: {customer.name}')
+
+
+@bot.message_handler(func=lambda m: True, state=States.sending_feedback)
+def feed_back_state(message: types.Message):
+    send_feedback_to_telegram(message.text.strip())
