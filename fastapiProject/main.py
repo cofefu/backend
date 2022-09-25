9ccebe5f5a8d6d2dda3285ecbbac2a820de540c7
hedgehog319 <fign1910@mail.ru>
2022-09-25 19:32:33 +1000

    * Cleanup

--- fastapiProject/main.py
+++ fastapiProject/main.py
@@ -33,8 +33,7 @@ app.add_middleware(
 )
 
 for app_ in APPS:
-    a = importlib.import_module(f'{app_}.urls')
-    router = getattr(a, 'router')
+    router = getattr(importlib.import_module(f'{app_}.urls'), 'router')
     if router is None:
         raise ImportError(f'The {app_} application does not contain "router"')
     app.include_router(router)
